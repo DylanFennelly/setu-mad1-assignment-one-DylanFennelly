c3def14393ec1a5998ddb21a15a9bbf362a3efa0
DylanFennelly <dylanantfennelly@gmail.com>
2023-10-28 14:05:15 +0100

    Added calculateHP utility + tests

--- src/test/kotlin/UtilitiesTest.kt
+++ src/test/kotlin/UtilitiesTest.kt
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.setu.character.console.helpers.calculateHP
 import org.setu.character.console.helpers.calculateMod
 import org.setu.character.console.models.CharacterModel
 
@@ -30,5 +31,13 @@ class UtilitiesTest {
         assertEquals(-2, calculateMod(7), "Incorrect ability score mod")        //case with minus and decimal
         assertEquals(-5, calculateMod(1), "Incorrect ability score mod")        //lower edge case
         assertEquals(10, calculateMod(30), "Incorrect ability score mod")        //upper edge case
+    }
+
+    @Test
+    fun testCalculateHP() {
+        assertEquals(10, calculateHP(1, "Bard", 14), "Incorrect max HP")             //level 1 bard, 14 con: 8 + 2 = 10     (simple case)
+        assertEquals(17, calculateHP(3, "Wizard", 12), "Incorrect max HP")           //level 3 wizard, 12 con: 6 + 1 + (4+1) * 2 = 17   (level up case)
+        assertEquals(345, calculateHP(20, "Barbarian", 30), "Incorrect max HP")      //level 20 barbarian, 30 con: 12 + 10 + (7+10)*19 = 345    (upper bound)
+        assertEquals(3, calculateHP(3, "Sorcerer", 1), "Incorrect max HP")           //level 3 sorcerer, 1 con: 6 - 5 + [(4-5) -1 < 1? 1] * 2 = 3   (negative HP per level case - defaults to 1 hp per level)
     }
 }
